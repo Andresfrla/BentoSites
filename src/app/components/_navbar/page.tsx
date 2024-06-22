5add47c4438fc111712cda5692ec84f9bec4f078
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-06-21 20:35:23 -0600

    fixed and styled the navbar

--- src/app/components/_navbar/page.tsx
+++ src/app/components/_navbar/page.tsx
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="navbar flex p-5 bg-primary text-base-100">
-    <div className="navbar-start">
+    <div className="navbar flex p-5 bg-primary text-base-100 fixed top-0 left-0 right-0 z-50 border-b-2 border-black">
+      <div className="navbar-start">
         <a className="btn btn-ghost text-2xl">BentoSites</a>
-    </div>
-    <div className="navbar-center hidden lg:flex">
+      </div>
+      <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 text-lg font-bold">
-        <li><a>Portafolio</a></li>
-        <li>
+          <li><a>Portafolio</a></li>
+          <li>
             <details>
-            <summary>Servicios</summary>
-            <ul className="p-2">
+              <summary>Servicios</summary>
+              <ul className="p-2">
                 <li><a>Pagina Web presentacional</a></li>
                 <li><a>Tienda virtual</a></li>
-            </ul>
+              </ul>
             </details>
-        </li>
-        <li><a>Blog</a></li>
-        <li><a>Sobre nosotros</a></li>
+          </li>
+          <li><a>Blog</a></li>
+          <li><a>Sobre nosotros</a></li>
         </ul>
-    </div>
-    <div className="navbar-end">
+      </div>
+      <div className="navbar-end">
         <a className="btn text-xl">Contactanos</a>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
